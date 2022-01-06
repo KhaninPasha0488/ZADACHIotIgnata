99b7435aacd7ef8c4bd735026b9999a58217f7e6
Pavel <93088269+KhaninPasha0488@users.noreply.github.com>
2022-01-06 23:27:29 +0300

    h1
    h1

--- HomeworksIgnat/homework/src/p2-homeworks/h1/HW1.tsx
+++ HomeworksIgnat/homework/src/p2-homeworks/h1/HW1.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import Message from "./Message";
+// import classes from './Message.module.css.'
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
     name: 'Some Name',
@@ -7,20 +8,20 @@ const messageData = {
     time: '22:00',
 }
 
-function HW1() {
+ function HW1() {
     return (
         <div>
             <hr/>
-            homeworks 1
+            {/*homeworks 1*/}
 
             {/*should work (должно работать)*/}
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
