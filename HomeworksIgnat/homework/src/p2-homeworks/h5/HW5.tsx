783eee43fb62ee124322ff86b7c83a2119d0515f
Pavel <93088269+KhaninPasha0488@users.noreply.github.com>
2022-02-23 14:33:51 +0300

    23-02 HW5

--- HomeworksIgnat/homework/src/p2-homeworks/h5/HW5.tsx
+++ HomeworksIgnat/homework/src/p2-homeworks/h5/HW5.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import Header from './Header'
-import Routes from './Routes'
+
+import {HashRouter} from "react-router-dom";
+import Routess from "./Routess";
 
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
+            <HashRouter>
 
             <Header/>
 
-            <Routes/>
+                {/* eslint-disable-next-line react/jsx-no-undef */}
+            <Routess/>
 
-            {/*</HashRouter>*/}
+            </HashRouter>
         </div>
     )
 }
