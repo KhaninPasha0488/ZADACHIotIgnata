90d5e58698e92565594958396a04c3f47ea13e9c
Pavel <93088269+KhaninPasha0488@users.noreply.github.com>
2022-04-14 12:21:12 +0300

    14.04

--- HomeworksIgnat/homework/src/p2-homeworks/h10/HW10.tsx
+++ HomeworksIgnat/homework/src/p2-homeworks/h10/HW10.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
-
+import s from './HW10.module.css'
 function HW10() {
     // useSelector, useDispatch
-    const loading = false
-
+    const [loading, setLoadings]  = useState(false)
+// const loading = false
     const setLoading = () => {
+        setLoadings(true)
+        setTimeout(()=>setLoadings(false),2000,)
+    }
+
         // dispatch
-        // setTimeout
-        console.log('loading...')
-    };
+
+
+
 
     return (
         <div>
@@ -19,7 +23,12 @@ function HW10() {
             {/*should work (должно работать)*/}
             {loading
                 ? (
-                    <div>крутилка...</div>
+                    <div className={s.preloader}>
+                        <div className={s.preloader__row}>
+                            <div className={s.preloader__item}></div>
+                            <div className={s.preloader__item}></div>
+                        </div>
+                    </div>
                 ) : (
                     <div>
                         <SuperButton onClick={setLoading}>set loading...</SuperButton>
